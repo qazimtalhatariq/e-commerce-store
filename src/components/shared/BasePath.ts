3fbb26af4eb063cab52f2bfe476ed505da4e054f
MUHAMMAD TALHA TARIQ <151367252+Talha9417@users.noreply.github.com>
2024-07-30 19:19:12 +0500

    update files

--- src/components/shared/BasePath.ts
+++ src/components/shared/BasePath.ts
@@ -1,6 +1,6 @@
 const BASE_PATH_FORAPI =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://commerece-e.vercel.app";
+    : "https://dine-mart.vercel.app";
 
 export default BASE_PATH_FORAPI;
